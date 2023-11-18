28ed93c8bf26fdc577a5a0c081d37d4d63cdabec
prashanthvaddepally <prashanth.privacy@gmail.com>
2023-11-18 13:51:06 +0000

    first of rest API

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+
 const path = require("path");
 const app = express();
+app.use(express.json());
 let db = null;
 const dbpath = path.join(__dirname, "todoApplication.db");
 const intializeAndRunServer = async () => {
@@ -20,3 +22,29 @@ const intializeAndRunServer = async () => {
   }
 };
 intializeAndRunServer();
+
+app.get("/todos/", async (request, response) => {
+  const {
+    offset = 0,
+    limit = 5,
+    order = "ASC",
+    order_by = "id",
+    search_q = "TO DO",
+  } = request.query;
+  const getquery = `
+    SELECT * FROM todo
+    WHERE status LIKE %'${search_q}'%
+    ORDER BY '${order_by}' '${order}'
+    LIMIT ${limit} OFFSET ${offset};`;
+  const thetodo = await db.all(getquery);
+  const answer = (thetodo) => {
+    return {
+      Id: thetodo.id,
+      Todo: thetodo.todo,
+      Priority: thetodo.priority,
+      Status: thetoedo.status,
+    };
+  };
+  response.send(thetodo.map((eachtodo) => ans(eachtodo)));
+});
+module.exports = app;
